b37272cdbcba6a3b860db9ebe0f6c92f2db7b50a
Valentin Payet <valentinpayet06@gmail.com>
2022-06-02 10:53:37 +0200

    mon clientssql

---
+++ clients.sql
@@ -0,0 +1,14 @@
+-- Authentification/connexion
+>mysql -u root
+>mysql -u root -p
+
+
+--- 1 Lister les bases de données
+
+SHOW DATABASES;
+
+-- 2. Création de la base de donées
+
+CREATE DATABASE ventes;
+
+-- 3 Création de la table clients
